230cbee692126356d9b169c006b4830107d27cde
Alberto Navarrete Martín <alnavart@gmail.com>
2016-03-08 21:30:22 +0100

    Build your first prototype and shout I can fly

---
+++ src/main/java/org/demo/workshop/MySpacecraft.java
@@ -0,0 +1,20 @@
+package org.demo.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MySpacecraft {
+
+    @RequestMapping("/")
+    String home() {
+        return "I can fly!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MySpacecraft.class, args);
+    }
+}
