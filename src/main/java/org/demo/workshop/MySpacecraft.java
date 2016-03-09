ac10cda38ffdab7e13522bf4000a6de3e756a462
Alberto Navarrete Martín <alnavart@gmail.com>
2016-03-09 22:31:34 +0100

    Fly high into space: Autodiscover with Eureka-netflix

--- src/main/java/org/demo/workshop/MySpacecraft.java
+++ src/main/java/org/demo/workshop/MySpacecraft.java
@@ -2,11 +2,13 @@ package org.demo.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class MySpacecraft {
 
     @RequestMapping("/")
